54cd0fc4cddd0e4fc0937c39e10a52f48a59ece2
Willem Achterhof <79074811+WillemAchterhof@users.noreply.github.com>
2026-08-07 13:00:42 +0200

    Add AG_DIR_UTILITIES variable to paths.sh

--- lib/core/variables/paths.sh
+++ lib/core/variables/paths.sh
@@ -34,6 +34,7 @@ readonly AG_DIR_CORE="$AG_DIR_LIB/core"
 readonly AG_DIR_INSTALLER="$AG_DIR_LIB/installer"
 readonly AG_DIR_MENU="$AG_DIR_LIB/menu"
 readonly AG_DIR_PREPARE="$AG_DIR_LIB/prepare"
+readonly AG_DIR_UTILITIES="$AG_DIR_LIB/utilities"
 readonly AG_DIR_VALIDATE="$AG_DIR_LIB/validate"
 # /ag_state
 readonly AG_DIR_PROFILE_STATE="$AG_DIR_STATE/profile"
