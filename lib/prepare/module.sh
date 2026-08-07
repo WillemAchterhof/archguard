915288329adc9ef1ca5cef997f5259bcfaa40f2e
WillemAchterhof <willem.achterhof@protonmail.com>
2026-08-07 18:53:03 +0200

    tet

--- lib/prepare/module.sh
+++ lib/prepare/module.sh
@@ -15,12 +15,13 @@
 #    - Source itself
 # ==============================================================================
 
-shopt -s globstar
+# shopt -s globstar
 
-for file in "$AG_DIR_PREPARE"/**/*.sh; do
+# for file in "$AG_DIR_PREPARE"/**/*.sh; do
+for file in "$AG_DIR_PREPARE/storage/disk/*.sh; do
     [[ -f "$file" ]] || continue
     [[ "$(basename "$file")" == "module.sh" ]] && continue
     source "$file"
 done
 
-shopt -u globstar
+# shopt -u globstar
