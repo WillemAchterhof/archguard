66e0c666c9711d44be6a94def960a9c1e02a10ad
WillemAchterhof <willem.achterhof@protonmail.com>
2026-08-12 08:29:48 +0200

    test

--- orchestrator/pipeline.sh
+++ orchestrator/pipeline.sh
@@ -36,7 +36,7 @@ run_pipeline()
         [[ "$AG_MENU_EXIT" == "1" ]] && { msg "Installer exited"; exit 0; }
         
         msg "Validate profile"
-        module_validate || true
+        module_validate
     
         [[ "$AG_MENU_PROCEED" == "1" ]] && break
     done
