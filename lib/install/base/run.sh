5f33f2eca500fef44f8c42bc90f1ca88ebe81f54
Willem Achterhof <79074811+WillemAchterhof@users.noreply.github.com>
2026-08-13 14:01:13 +0200

    Add run.sh for base installation process

---
+++ lib/install/base/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# ==============================================================================
+#  Arch Secure Installer V2.6 — Disk Preparation Entry Point
+# ==============================================================================
+#  lib/install/base/run.sh
+# ==============================================================================
+
+run_install_base()
+{
+    install_base
+
+    msg "Base Install completed"
+}
