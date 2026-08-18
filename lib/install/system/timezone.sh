50b8079abd469851ab73cd31eda08fe90c9a1dd6
WillemAchterhof <willem.achterhof@protonmail.com>
2026-08-18 12:50:19 +0200

    test

--- lib/install/system/timezone.sh
+++ lib/install/system/timezone.sh
@@ -5,7 +5,7 @@
 #  lib/install/system/timezone.sh
 #
 #  Requires:
-#    - run_chroot()
+#    - AG_INSTALL_ROOT
 #    - AG_P_TIMEZONE
 # ==============================================================================
 
@@ -13,9 +13,11 @@ configure_timezone()
 {
     msg "Configuring timezone: $AG_P_TIMEZONE"
 
-    run_chroot ln -sf \
+    ln -sf \
         "/usr/share/zoneinfo/$AG_P_TIMEZONE" \
-        /etc/localtime
+        "$AG_INSTALL_ROOT/etc/localtime"
 
     run_chroot hwclock --systohc
+
+    msg "Timezone configured"
 }
