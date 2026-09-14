98c01720c1c6012b1fa122eda18a6aef0d214eec
Willem Achterhof <79074811+WillemAchterhof@users.noreply.github.com>
2026-09-14 11:38:48 +0200

    Update usbgaurd.sh

--- lib/postboot/install/base/usbgaurd.sh
+++ lib/postboot/install/base/usbgaurd.sh
@@ -14,6 +14,26 @@ usbguard_policy() {
     usbguard generate-policy > /etc/usbguard/rules.conf
 }
 
+usbguard_ipc() {
+    local acl_dir="/etc/usbguard/IPCAccessControl.d"
+    local target_user="$AG_P_USERNAME"
+
+    [[ -n "${AG_P_USERNAME:-}" ]] \
+        || fatal "AG_P_USERNAME is not set"
+
+    printf "[*] Configuring USBGuard IPC access for %s..." "$target_user"
+
+    mkdir -p "$acl_dir"
+
+    cat > "$acl_dir/$target_user" <<EOF
+Devices=list
+Devices=modify
+EOF
+
+    chmod 600 "$acl_dir/$target_user"
+    chown root:root "$acl_dir/$target_user"
+}
+
 usbguard_turn_on() {
     printf "[*] Enabling USBGuard..."
     systemctl enable usbguard
@@ -25,5 +45,6 @@ usbguard_turn_on() {
 base_usbguard() {
     usbguard_install
     usbguard_policy
+    usbguard_ipc
     usbguard_turn_on
 }
